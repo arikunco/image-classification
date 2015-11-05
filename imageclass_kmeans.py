0f5c96cc69813ef0984ebb2d9cfe4c5c8717eb6a
Bernardus Ari Kuncoro <ari.kuncoro1987@gmail.com>
2015-11-05 20:28:30 +0700

    aaa

---
+++ imageclass_kmeans.py
@@ -0,0 +1,45 @@
+import pylab as pl
+from numpy import genfromtxt
+from sklearn import svm, metrics
+from sklearn.neighbors import KNeighborsClassifier
+import scipy.io
+from k_means import kmeans
+
+data = scipy.io.loadmat('dataku.mat')["dataimage_plus"]
+
+#print(data)
+
+#data = genfromtxt('dataimage.csv', delimiter=',')
+n_samples = len(data)
+#split data input and data output
+data_i = data[:,0:2] #2:4 => fft (mean dan standard deviasi FFT), 0:2 => (entropy dan energi GLCM)
+
+data_o,c = kmeans(data_i,3)
+print len(data_o)
+data_expected = data[:,4] - 1
+print len(data_expected)
+
+#split 50 % data (data training)
+# data_i_train = data_i[0:][::2]
+# data_o_train = data_o[0:][::2]
+
+# #split 50 % data (data testing)
+# data_i_test = data_i[1:][::2]
+# data_o_test = data_o[1:][::2]
+
+# #create knn classifier
+# neigh = KNeighborsClassifier(n_neighbors=3)
+
+# #We learn the digits on the first half of the digits
+# neigh.fit(data_i_train, data_o_train)
+
+# # Now predict the value of the digit on the second half:
+expected = data_expected
+predicted = data_o
+
+print predicted
+print expected
+
+print("Classification report for classifier %s:\n%s\n"
+     % ("K-Means", metrics.classification_report(expected, predicted)))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
